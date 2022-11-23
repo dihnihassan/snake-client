2cdad407be75f6c49979df0ec42fbcc267922b83
Dihni Hassan <dihnihassan@gmail.com>
2022-11-23 18:22:27 -0500

    Moved setupInput and handleUserInput to input.js

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 // const net = require("net");
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 //establishes a connection with the game server
 // const connect = function () {
@@ -24,20 +25,20 @@ const {connect} = require('./client');
 
 // };
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
- stdin.on("data", handleUserInput);
- return stdin;
-};
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//  stdin.on("data", handleUserInput);
+//  return stdin;
+// };
 
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function(data) {
+//   if (data === '\u0003') {
+//     process.exit();
+//   }
+// };
 
 console.log("Connecting ...");
 connect();
